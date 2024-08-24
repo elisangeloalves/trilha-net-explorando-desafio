acfc632a185922a55efc88e1211050a719f11bf2
elisangeloalves <preto115@gmail.com>
2024-08-24 01:55:29 -0300

    feat: implementacao do desafio reserva de hotel

--- Program.cs
+++ Program.cs
@@ -6,20 +6,51 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
+string premium = "Premium";
+string luxo = "Luxo";
+
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suitePremium = new Suite(tipoSuite: premium, capacidade: 5, valorDiaria: 30);
+Suite suiteLuxo = new Suite(tipoSuite: luxo, capacidade: 2, valorDiaria: 15);
+Suite[] suites = {suiteLuxo, suitePremium};
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+Reserva reserva1 = new Reserva(diasReservados: 5);
+Reserva reserva2 = new Reserva(diasReservados: 10);
+Reserva[] reservas = {reserva1, reserva2};
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+for(int i = 0; i < reservas.Count(); i++)
+{
+    try
+    {
+        Console.WriteLine("====================================");
+        Console.WriteLine($"Reserva {i+1}");
+        Console.WriteLine($"\nSuite: {suites[i].TipoSuite}");
+        Console.WriteLine($"Capacidade: {suites[i].Capacidade} hóspedes");
+        reservas[i].CadastrarSuite(suites[i]);
+        reservas[i].CadastrarHospedes(hospedes);
+    
+        // Exibe a quantidade de hóspedes e o valor da diária
+        Console.WriteLine($"Hóspedes: {reservas[i].ObterQuantidadeHospedes()}");
+        Console.WriteLine($"Valor total da diária: {reservas[i].CalcularValorDiaria():C}");
+
+        Console.WriteLine("Reserva realizada com sucesso!");
+    } 
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Reserva cancelada!");
+    }
+    finally
+    {
+        Console.WriteLine("\nObrigado por usar nossos serviços!");
+    }
+}
